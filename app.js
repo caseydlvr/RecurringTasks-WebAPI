126d81b078b81c1258f1d31b225d74ff4b5bcc39
Casey de la Vega-Rawson <spamcasey@gmail.com>
2019-01-17 02:50:34 -0800

    objection imports

--- app.js
+++ app.js
@@ -2,8 +2,13 @@
 
 const express = require('express');
 const routes = require('./routes');
+const bodyParser = require('body-parser');
+const Knex = require('knex');
+const knexConfig = require('./knexfile');
+const { Model } = require('objection');
 
 const app = express();
 const port = process.evn.PORT || 3000;
+const knex = Knex(knexConfig.development);
 
 app.listen(port, () => console.log(`Express server is listening on port ${port}`));
