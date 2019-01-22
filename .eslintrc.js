6729e0c8f40b5aab7c8d3adb12a6d236405c2544
Casey de la Vega-Rawson <spamcasey@gmail.com>
2019-01-21 20:52:09 -0800

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
     extends: 'airbnb-base',
+    overrides: [
+      {
+        files: ['migrations/*.js'],
+        rules: {
+          'arrow-body-style': 'off'
+        },
+      },
+    ],
     parserOptions: {
-        sourceType: 'script'
-    }
+        sourceType: 'script',
+    },
 };
