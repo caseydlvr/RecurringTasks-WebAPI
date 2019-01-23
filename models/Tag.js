9111ca508165fda182e9225032b1233f1dbf9dfc
Casey de la Vega-Rawson <spamcasey@gmail.com>
2019-01-22 19:21:48 -0800

    jsonSchema for Tag

--- models/Tag.js
+++ models/Tag.js
@@ -6,6 +6,19 @@ class Tag extends Model {
   static get tableName() {
     return 'tags';
   }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['user_id', 'name'],
+
+      properties: {
+        id: { type: 'integer' },
+        user_id: { type: 'integer', minimum: 1 },
+        name: { type: 'string', minLength: 1, maxLength: 255 },
+      },
+    };
+  }
 }
 
 module.exports = Tag;
