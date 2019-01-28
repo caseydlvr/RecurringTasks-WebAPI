d8fb1801693ef34f1fb172db09f71c7ce65812fc
Casey de la Vega-Rawson <spamcasey@gmail.com>
2019-01-27 23:53:22 -0800

    tweak production DB settings for Heroku

--- knexfile.js
+++ knexfile.js
@@ -14,10 +14,8 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      host: process.env.DATABASE_URL,
-      database: 'recurring_tasks',
-    },
+    connection: process.env.DATABASE_URL,
+    ssl: true,
     pool: {
       min: 2,
       max: 10,
