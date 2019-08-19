1e8af7069a9fe1b9353061d68a67de02fed3fda2
Casey de la Vega-Rawson <spamcasey@gmail.com>
2019-08-19 01:30:56 -0600

    use env for dev host and database name

--- knexfile.js
+++ knexfile.js
@@ -7,10 +7,10 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      host: '127.0.0.1',
+      host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: 'recurring_tasks',
+      database: process.env.DB_NAME,
     },
   },
 
