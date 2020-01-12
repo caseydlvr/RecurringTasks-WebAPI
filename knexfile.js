df8340c573140e4330bbadf698a24fa86c912b05
Casey de la Vega-Rawson <spamcasey@gmail.com>
2020-01-11 18:13:58 -0700

    use pg instead of postgresql for db client

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 module.exports = {
 
   development: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
@@ -15,7 +15,7 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     ssl: true,
   },
